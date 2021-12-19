1b40edcacddadf6fbb2c1b4c4634166e49819cec
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-12-19 09:15:47 +0000

    added blog section

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
 ////////////////////////////////
 // Typing Effect in Hero
-const texts = ['Product Owner', 'Project Manager'];
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
 
-// IIFE to call the function immediately
-(function type() {
-    if(count === texts.length) {
-        count = 0;
+// Type method
+TypeWriter.prototype.type = function() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        // Add  char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index)
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    document.querySelector('.typing').textContent = letter;
-    if(letter.length === currentText.length) {
-        count++;
-        index = 0;
-    };
+    setTimeout(() => this.type(), 500)
+}
 
-    setTimeout(type, 500);
-}());
+// Init on DOM load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init app
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // Init Typewriter
+    new TypeWriter(txtElement, words, wait);
+}
+
+
+// const texts = ['Product Owner', 'Project Manager'];
+// let count = 0;
+// let index = 0;
+// let currentText = '';
+// let letter = '';
+
+// // IIFE to call the function immediately
+// (function type() {
+//     if(count === texts.length) {
+//         count = 0;
+//     }
+
+//     currentText = texts[count];
+//     letter = currentText.slice(0, ++index)
+
+//     document.querySelector('.typing').textContent = letter;
+//     if(letter.length === currentText.length) {
+//         count++;
+//         index = 0;
+//     };
+
+//     setTimeout(type, 300);
+// }());
 
 
 ////////////////////////////////
